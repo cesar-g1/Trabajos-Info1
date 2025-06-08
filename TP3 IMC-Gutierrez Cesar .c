a1823316e7103468b3fc46c682554768ea689c40
Gutiérrez Cesar <138553082+cesar-g1@users.noreply.github.com>
2025-06-08 15:36:08 -0300

    cambie el codigo como indica el punto 2 del tp4

--- TP3 IMC-Gutierrez Cesar .c
+++ TP3 IMC-Gutierrez Cesar .c
@@ -20,6 +20,18 @@ int main() {
 		printf("18.5 - 24.9 | Normal\n");
 		printf("25.0 - 29.9 | Sobrepeso\n");
 		printf(">=30   | Obesidad\n");
+	
+	printf("\nSu condición según el BMI es: ");
+	if (bmi < 18.5) {
+		printf("Bajo peso\n");
+	} else if (bmi < 25.0) {
+		printf("Normal\n");
+	} else if (bmi < 30.0) {
+		printf("Sobrepeso\n");
+	} else {
+		printf("Obesidad\n");
+	}
+	
 		
 		return 0;
 	
